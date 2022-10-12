4d348f8b2c0ea8d38a13db5644e0d5049ec8bf98
Marcus Haddon <haddon.marcus@gmail.com>
2022-10-11 20:58:37 -0700

    find working architecture for now

--- src/sketch/sketch.ts
+++ src/sketch/sketch.ts
@@ -15,12 +15,12 @@ import {
   multConst,
   Signal,
 } from "../lib/signals";
-import { grid } from "../lib/space";
+import { grid, mutateCoord } from "../lib/space";
 
 type Model = {
   steadyBreeze: Signal;
   gust: Signal;
-  windDir: P5.Vector;
+  windDir: Coord;
 }
 
 const ratios: [number, number][] = [
@@ -34,72 +34,64 @@ type LayerGroup = {
   layer: Leaf[]
 };
 
+const BREEZE_FREQ = 6;
 const model: Model = {
-  steadyBreeze: sin(0.25, 0, 1),
+  steadyBreeze: sin(BREEZE_FREQ, 0, 1),
   gust: floor(0, addConst(-4, sin(0.05, 0, 5))),
-  windDir: new P5.Vector(-1, 0)
+  windDir: [-1, 0]
 };
 
-
+const LAYER_COUNT = 3;
 
 const sketch = (p5: P5): Sketch => {
   const createLayer = (idx: number): Leaf[] => {
     const count = Math.floor(Math.pow(idx * 10, 1.5));
     const size = 200 / idx;
-    const green = 255 - 180 / idx;
+    const green = 255 - 50 * idx;
 
-    // const color = new P5.Color();
-    // color.setGreen(green);
+    const color = window.p5.color(0, green, 0);
+
     return grid(1000, 1000, count)
-      .map(([x, y]) => new Leaf(x, y, size));
+      .map(([ x, y ]) => new Leaf(x, y, size, color))
+      .sort(() => Math.random() > 0.5 ? -1 : 1);
   }
-  
-  // Apply translations based on layer idx
-  const wavesForLayer = curry((model: Model, layer: Leaf[]): LayerGroup => {
-    // BOOKMARK: alter waves
-    return {
-      model: {
-        ...model,
-      },
-      layer,
-    }
-  });
-  
-  const wfLayer = wavesForLayer(model);
-  
-  const wavesForLeaf = curry(({ layer, model }: LayerGroup): Sketch => {
-    // TODO: bind waves to leaf
-    return {
-      objects: layer,
-      tick: (time: number) => null
-    }
-  });
-  
-  const mergeSketches = (a: Sketch, b: Sketch): Sketch => ({
-    objects: [...a.objects, ...b.objects],
-    tick: (time: number) => {
-      a.tick(time);
-      b.tick(time);
-    }
-  });
-  
-  const blankSketch: Sketch = {
-    objects: [],
-    tick: (time: number) => null
-  };
-  
-  const LAYER_COUNT = 2;
-  
-  const scene = pipe(
-    range(0),
-    map(createLayer),
-    map(wfLayer),
-    map(wavesForLeaf),
-    reduce((acc, curr) => mergeSketches(acc, curr), blankSketch)
-  );
 
-  
-  return scene(LAYER_COUNT + 1);
+  const animateLayer = (leaves: Leaf[], layerIdx: number) => {
+    const layerBreezeRotation = 
+      BREEZE_FREQ * 0.25 * layerIdx;
+
+
+    return leaves.map(leaf => {
+      const leafBreezeRotation = layerBreezeRotation + leaf.position.x / 500;
+      const steadyBreeze = rotate(
+        leafBreezeRotation,
+        model.steadyBreeze
+      );
+      const [x, y] = model.windDir;
+      const dir = window.p5.createVector(x, y);
+
+      return (time: number) => {
+        const steadyBreezeLevel = steadyBreeze(time);
+        const breezeMovement = P5.Vector.mult(dir, steadyBreezeLevel);
+        leaf.translate(breezeMovement);
+      }
+    })
+  };
+
+  const layers = range(1, LAYER_COUNT + 1)
+    .map(idx => createLayer(idx));
+
+  const animations = layers
+    .map((layer, idx) => animateLayer(layer, idx))
+    .flatMap(tick => tick);
+
+  const tick = (time: number) => animations.forEach(a => a(time));
+
+
+  return {
+    objects: layers.reverse().flatMap(leaf => leaf),
+    tick
+  }
 }
 
 export default sketch;
